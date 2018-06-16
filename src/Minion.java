82e06376096a703a099d54f846dd155c399a568f
League <League@adminisorsimac2.attlocal.net>
2018-06-16 11:27:17 -0700

    Minion, part of popcorn

---
+++ src/Minion.java
@@ -0,0 +1,30 @@
+
+public class Minion {
+	private String name; 
+	 		private int eyes; 
+	  		private String color; 
+	  		private String master;
+	  Minion(String name) {
+	  			this.name = name;
+	  			}
+	  		public Minion(String string, int i, String string2, String string3) {
+		// TODO Auto-generated constructor stub
+	}
+			public String getName() {
+	  			return "My name is " + name + ".";
+	  		}
+	  		public String getEyes() {
+	  			return "Blue";
+	  		}
+	  		public String getColor() {
+				return color;	
+	  		}
+	  		public String setMaster() {
+	  			return master;
+	  		}
+	  		public String getMaster() {
+	  			return master;
+	  		}
+	  		
+}
+
---
+++ src/PopcornMaker.java
@@ -0,0 +1,7 @@
+
+public class PopcornMaker {
+public static void main(String[] args) {
+	Popcorn Good = new Popcorn("Good");
+	putInMicrowave(Good);
+}
+}

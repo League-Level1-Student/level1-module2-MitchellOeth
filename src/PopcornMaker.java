44c8df1836ade562c505c9374fd19d0795842635
league <league@worthymini-5.attlocal.net>
2018-06-17 11:25:27 -0700

    Tea, Pop

--- src/PopcornMaker.java
+++ src/PopcornMaker.java
@@ -1,7 +1,14 @@
+import javax.swing.JOptionPane;
 
 public class PopcornMaker {
 public static void main(String[] args) {
-	Popcorn Good = new Popcorn("Good");
-	putInMicrowave(Good);
+	String Goood = JOptionPane.showInputDialog("What flavor of popcorn do you want?");
+	String time = JOptionPane.showInputDialog("How many minutes do you want your popcorn to heat?");
+	Popcorn Good = new Popcorn(Goood);
+	Microwave Cool = new Microwave();
+	int tiime = Integer.parseInt(time);
+Cool.putInMicrowave(Good);	
+Cool.setTime(tiime);
+Cool.startMicrowave();
 }
 }
